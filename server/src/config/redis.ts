1c431b754e6fddf9aa933536f4458b04c975e111
BayajidAlam <bayajidalam2001@gmail.com>
2025-01-13 12:55:33 +0600

    Fix: Refactor redis-config for ec2

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,14 +1,27 @@
 import Redis from "ioredis";
 
 export const createRedisClient = () => {
-  const redisCluster = new Redis.Cluster([
-    { host: process.env.REDIS_HOST || "localhost", port: 6379 },
-    { host: process.env.REDIS_HOST || "localhost", port: 6380 },
-    { host: process.env.REDIS_HOST || "localhost", port: 6381 },
-    { host: process.env.REDIS_HOST || "localhost", port: 6382 },
-    { host: process.env.REDIS_HOST || "localhost", port: 6383 },
-    { host: process.env.REDIS_HOST || "localhost", port: 6384 },
-  ]);
+
+  //for docker
+  // const redisCluster = new Redis.Cluster([
+  //   { host: process.env.REDIS_HOST || "localhost", port: 6379 },
+  //   { host: process.env.REDIS_HOST || "localhost", port: 6380 },
+  //   { host: process.env.REDIS_HOST || "localhost", port: 6381 },
+  //   { host: process.env.REDIS_HOST || "localhost", port: 6382 },
+  //   { host: process.env.REDIS_HOST || "localhost", port: 6383 },
+  //   { host: process.env.REDIS_HOST || "localhost", port: 6384 },
+  // ]);
+
+
+const redisCluster = new Redis.Cluster([
+  { host: process.env.REDIS_1, port: 6379 },
+  { host: process.env.REDIS_2, port: 6379 },
+  { host: process.env.REDIS_3, port: 6379 },
+  { host: process.env.REDIS_4, port: 6379 },
+  { host: process.env.REDIS_5, port: 6379 },
+  { host: process.env.REDIS_6, port: 6379 }
+]);
+
 
   // Check if connected
   redisCluster.on("connect", () => {
